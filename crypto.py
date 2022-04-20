a00c9466c369220cad9c57987665e853579e8dd8
David14334 <98358777+David14334@users.noreply.github.com>
2022-04-19 23:38:40 -0500

    project

--- crypto.py
+++ crypto.py
@@ -15,10 +15,10 @@ mycellphone = '+18325097076'
 webpage = 'https://coinmarketcap.com/'
 
 page = urlopen(webpage)			
-
 soup = BeautifulSoup(page, 'html.parser')
 
 title = soup.title
+
 
 
 
@@ -31,25 +31,25 @@ for x in range (1,6):
     name = cols[1].text
     current_price = cols[2].text.replace(',', '').replace('$','')
     current_price = float(current_price)
-    change_percentage = cols[3].text
-    change_float = float(change_percentage.text.strip('%'))
+    percent_change = cols[3].text
+    change_float = float(percent_change.text.strip('%'))
     old_price = round(current_price / (1+ (change_float/100)), 2)
 
     print(f"Rank : {rank}")
     print(f"Name: {name}")
     print(f"Current Price: {current_price}")
     print(f"Old Price: {old_price}")
-    print(f"% Change in last 24 hours: {change_percentage}")
+    print(f"% Change in last 24 hours: {percent_change}")
 
     if name == "Bitcoin":
         btc = current_price
         if btc < 40000:
-            textmessage = client.messages.create(to  = mycellphone, from_= TwilioNumber, 
+            textmessage = client.messages.create(to = mycellphone, from_= TwilioNumber, 
             body ='BTC is below $40,000')
     if name == "Ethereum":
         eth = current_price
         if eth < 3000:
-            textmessage = client.messages.create(to  = mycellphone, from_= TwilioNumber, 
+            textmessage = client.messages.create(to = mycellphone, from_= TwilioNumber, 
             body ='ETH is below $3,000')
  
     input()
